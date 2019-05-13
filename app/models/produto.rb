65f0f4bb1528e072e757b87e84a9a7590809cc0f
julianapereira3 <julianna.lopes96@gmail.com>
2019-05-13 14:09:12 -0300

    Adicionando correção ao arquivo (produtos.rb)

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -1,5 +1,6 @@
 class Produto < ApplicationRecord
 	belongs_to :departamento, optional: true
-    validates :preco, :departamento presence: true
+    validates :preco, :departamento, presence: true
 	validates :nome, length: { minimum: 4}
+
 end
