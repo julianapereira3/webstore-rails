97152d905e15cb9a86c91ebcef4781182d1ea97d
julianapereira3 <julianna.lopes96@gmail.com>
2019-05-13 11:06:19 -0300

    Atualização e Validação da aba Departamentos

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -1,6 +1,5 @@
 class Produto < ApplicationRecord
-	 belongs_to :departamento, optional: true
-
-    validates :preco, presence: true
+	belongs_to :departamento, optional: true
+    validates :preco, :departamento presence: true
 	validates :nome, length: { minimum: 4}
 end
